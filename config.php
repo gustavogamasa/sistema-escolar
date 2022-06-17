302a765b074d1a1b01e74fb2f0f147248f38d56d
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-06-17 01:06:24 -0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome_escola = "Escola genérica";
+$url = "http://127.0.0.1/escolar";
+$endereco_escola = "Rua 123";
+$telefone_escola =  "123123123";
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "sistema_escolar";
+
+
+?>
