3608b7ef4e8388a562cadf254eca799545771909
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-07-21 14:18:09 -0300

    Foto - Insert banco

--- painel-adm/professores/inserir.php
+++ painel-adm/professores/inserir.php
@@ -39,6 +39,26 @@ if($antigo != $cpf){
 }
 
 
+//SCRIPT PARA SUBIR FOTO NO BANCO
+$nome_img = preg_replace('/[ -]+/' , '-' , @$_FILES['imagem']['name']);
+$caminho = '../../img/professores/' .$nome_img;
+if (@$_FILES['imagem']['name'] == ""){
+  $imagem = "sem-foto.jpg";
+}else{
+    $imagem = $nome_img;
+}
+
+$imagem_temp = @$_FILES['imagem']['tmp_name']; 
+$ext = pathinfo($imagem, PATHINFO_EXTENSION);   
+if($ext == 'png' or $ext == 'jpg' or $ext == 'jpeg' or $ext == 'gif'){ 
+move_uploaded_file($imagem_temp, $caminho);
+}else{
+	echo 'Extensão de Imagem não permitida!';
+	exit();
+}
+
+
+
 //VERIFICAR SE O REGISTRO COM MESMO EMAIL JÁ EXISTE NO BANCO
 if($antigo2 != $email){
 	$query = $pdo->query("SELECT * FROM professores where email = '$email' ");
@@ -52,7 +72,7 @@ if($antigo2 != $email){
 
 
 if($id == ""){
-	$res = $pdo->prepare("INSERT INTO professores SET nome = :nome, cpf = :cpf, email = :email, endereco = :endereco, telefone = :telefone");	
+	$res = $pdo->prepare("INSERT INTO professores SET nome = :nome, cpf = :cpf, email = :email, endereco = :endereco, telefone = :telefone, foto = '$foto'");	
 
 	$res2 = $pdo->prepare("INSERT INTO usuarios SET nome = :nome, cpf = :cpf, email = :email, senha = :senha, nivel = :nivel");	
 	$res2->bindValue(":senha", '123');
