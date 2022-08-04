2e992403e51542b366f26d39e4c8498976e7f499
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-08-04 19:49:09 -0300

    Tesoureiros - Insert e Drop

--- painel-adm/secretarios.php
+++ painel-adm/secretarios.php
@@ -6,7 +6,9 @@ require_once("../conexao.php");
 @session_start();
 
     //verificar se o usuário está autenticado
+    
 if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'admin'){
+
     echo "<script language='javascript'> window.location='../index.php' </script>";
 
 }
