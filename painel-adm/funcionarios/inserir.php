05d6f6c845e72b9067ad9515475ce10b46b1c5a1
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-08-10 00:38:43 -0300

    Funcionarios
    CRUD ok

--- painel-adm/funcionarios/inserir.php
+++ painel-adm/funcionarios/inserir.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-require_once("../../conexao.php"); 
+require_once("../../conexao.php");
 
 $nome = $_POST['nome'];
 $telefone = $_POST['telefone'];
@@ -13,53 +13,49 @@ $antigo = $_POST['antigo'];
 $antigo2 = $_POST['antigo2'];
 $id = $_POST['txtid2'];
 
-if($nome == ""){
+if ($nome == "") {
 	echo 'O nome é Obrigatório!';
 	exit();
 }
 
-if($email == ""){
+if ($email == "") {
 	echo 'O email é Obrigatório!';
 	exit();
 }
 
-if($cpf == ""){
+if ($cpf == "") {
 	echo 'O CPF é Obrigatório!';
 	exit();
 }
 
-//VERIFICAR SE O REGISTRO JÁ EXISTE NO BANCO
-if($antigo != $cpf){
+//VERIFICAR SE O REGISTRO JÁ EXISTE NO BANCO - Funcionarios
+if ($antigo != $cpf) {
 	$query = $pdo->query("SELECT * FROM funcionarios where cpf = '$cpf' ");
 	$res = $query->fetchAll(PDO::FETCH_ASSOC);
 	$total_reg = @count($res);
-	if($total_reg > 0){
+	if ($total_reg > 0) {
 		echo 'O CPF já está Cadastrado!';
 		exit();
 	}
 }
 
 
-//VERIFICAR SE O REGISTRO COM MESMO EMAIL JÁ EXISTE NO BANCO
-if($antigo2 != $email){
+//VERIFICAR SE O REGISTRO COM MESMO EMAIL JÁ EXISTE NO BANCO - Funcionarios
+if ($antigo2 != $email) {
 	$query = $pdo->query("SELECT * FROM funcionarios where email = '$email' ");
 	$res = $query->fetchAll(PDO::FETCH_ASSOC);
 	$total_reg = @count($res);
-	if($total_reg > 0){
+	if ($total_reg > 0) {
 		echo 'O Email já está Cadastrado!';
 		exit();
 	}
 }
 
 
-if($id == ""){
-	$res = $pdo->prepare("INSERT INTO funcionarios SET nome = :nome, cpf = :cpf, email = :email, endereco = :endereco, telefone = :telefone, cargo = :cargo");	
-
-
-
-}else{
+if ($id == "") {
+	$res = $pdo->prepare("INSERT INTO funcionarios SET nome = :nome, cpf = :cpf, email = :email, endereco = :endereco, telefone = :telefone, cargo = :cargo");
+} else {
 	$res = $pdo->prepare("UPDATE funcionarios SET nome = :nome, cpf = :cpf, email = :email, endereco = :endereco, telefone = :telefone, cargo = :cargo WHERE id = '$id'");
-	
 }
 
 $res->bindValue(":nome", $nome);
@@ -69,12 +65,7 @@ $res->bindValue(":email", $email);
 $res->bindValue(":endereco", $endereco);
 $res->bindValue(":cargo", $cargo);
 
-
-
-
 $res->execute();
 
 
 echo 'Salvo com Sucesso!';
-
-?>
