c3a2f08ea1c2568e5b313e91b844f53b5f974680
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2022-11-16 23:16:56 -0300

    Backup banco de dados
    Alterações do CRUD

--- backup-banco/sistema_escolar.sql
+++ backup-banco/sistema_escolar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 23, 2022 at 06:14 AM
+-- Generation Time: Nov 17, 2022 at 03:16 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.2.34
 
@@ -46,9 +46,12 @@ CREATE TABLE `alunos` (
 --
 
 INSERT INTO `alunos` (`id`, `nome`, `cpf`, `telefone`, `email`, `endereco`, `responsavel`, `data_nascimento`, `data_cadastro`, `foto`, `sexo`) VALUES
-(1, 'Aluno 1', '133.333.333-33', '(12) 31231-2312', 'aluno1@sistema.com', 'Rua bem Legal mas sem almondega', NULL, '1995-01-01', '2022-09-05', 'sem-foto.jpg', 'M'),
-(2, 'Aluno 2', '132.222.222-22', '(12) 31231-31', 'aluno2@sistema.com', 'Endereço bem louco', NULL, '2022-09-05', '2022-09-05', 'jimmy.jpeg', 'F'),
-(4, 'ASdAsdasd', '123.123.123-12', '(12) 31231-231', 'DSgsfsdf', '123123123', '123.981.763-86', '2022-09-23', '2022-09-23', 'sem-foto.jpg', 'M');
+(1, 'Felipe Santos', '788.888.888-88', '(99) 99999-9999', 'felipe@hotmail.com', 'Rua Almeida Campos 150', '', '2000-11-16', '2020-11-16', 'team-1.jpg', 'M'),
+(2, 'Mariano Campos', '789.555.555-55', '(55) 55555-5555', 'mariano@hotmail.com', 'Rua Almeida Campos 145', '', '2001-11-16', '2020-11-16', 'usuario-icone.jpg', 'M'),
+(3, 'Marina Silva', '875.555.555-55', '(55) 55555-5555', 'marina@hotmail.com', 'Rua C', '', '2000-11-16', '2020-11-16', 'team-2.jpg', 'F'),
+(5, 'Rui Costaa', '488.888.888-88', '(33) 33333-3333', 'rui@hotmail.com', 'Rua Almeida Campos 150', '222.222.222-22', '2002-11-17', '2020-11-17', 'sem-foto.jpg', 'M'),
+(6, 'Outro aluno', '123.123.131-12', '(11) 11111-1111', 'giosadig@email.com', '', '', '1993-02-10', '2022-11-16', 'sem-foto.jpg', 'M'),
+(7, 'Teste 123123', '111.111.111-11', '', 'teste@procemsoeifj.com', '', '', '1995-03-03', '2022-11-16', 'sem-foto.jpg', 'M');
 
 -- --------------------------------------------------------
 
@@ -66,8 +69,9 @@ CREATE TABLE `disciplinas` (
 --
 
 INSERT INTO `disciplinas` (`id`, `nome`) VALUES
-(5, 'Disciplina 1'),
-(6, 'Disciplina 2');
+(1, 'Programação WEB'),
+(2, 'WEB Designer'),
+(4, 'Design Gráfico');
 
 -- --------------------------------------------------------
 
@@ -79,18 +83,43 @@ CREATE TABLE `funcionarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `cpf` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `endereco` varchar(100) DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
-  `cargo` varchar(30) NOT NULL
+  `cargo` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `funcionarios`
 --
 
-INSERT INTO `funcionarios` (`id`, `nome`, `cpf`, `telefone`, `endereco`, `email`, `cargo`) VALUES
-(1, 'Funcionario 1', '131.111.111-11', '(11) 11111-1111', 'Endereço 1', 'func1@email.com', 'Cargo 1');
+INSERT INTO `funcionarios` (`id`, `nome`, `cpf`, `email`, `telefone`, `endereco`, `cargo`) VALUES
+(1, 'Matheus Santos', '788.888.888-88', 'mateus@hotmail.com', '(99) 99999-9999', 'Rua X', 'Porteiro'),
+(2, 'Talita Silva', '899.999.999-99', 'talita@hotmail.com', '(99) 99999-9999', 'Rua Almeida Campos 150', 'Cantineira');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matriculas`
+--
+
+CREATE TABLE `matriculas` (
+  `id` int(11) NOT NULL,
+  `turma` int(11) NOT NULL,
+  `aluno` int(11) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `matriculas`
+--
+
+INSERT INTO `matriculas` (`id`, `turma`, `aluno`, `data`) VALUES
+(1, 1, 5, '2020-11-17'),
+(3, 1, 1, '2020-11-17'),
+(6, 2, 1, '2020-11-17'),
+(7, 1, 2, '2020-11-17'),
+(8, 2, 3, '2020-11-17');
 
 -- --------------------------------------------------------
 
@@ -102,7 +131,7 @@ CREATE TABLE `professores` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `cpf` varchar(20) NOT NULL,
-  `telefone` varchar(50) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `endereco` varchar(100) DEFAULT NULL,
   `foto` varchar(100) DEFAULT NULL
@@ -113,9 +142,8 @@ CREATE TABLE `professores` (
 --
 
 INSERT INTO `professores` (`id`, `nome`, `cpf`, `telefone`, `email`, `endereco`, `foto`) VALUES
-(1, 'Professor 1', '111.111.111-11', '(11) 11111-1111', 'professor1@email.com', 'Endereço 2', 'jimmy.jpeg'),
-(12, 'Professor 2', '222.222.222-22', '(22) 22222-2222', '2@email.com', 'Endereço 2', 'IMG_4654.jpg'),
-(13, 'Professor 3', '333.333.333-33', '(33) 33333-3333', '3@email.com', 'Endereço 3', 'sem-foto.jpg');
+(3, 'Professor Teste', '777.777.777-77', '(77) 77777-7777', 'professor@hotmail.com', 'Rua Almeida Campos 150', 'usuario-icone.jpg'),
+(4, 'Hugo Vasconcelos', '788.888.888-88', '(88) 88888-8888', 'hugovasconcelosf@hotmail.com', 'Rua Almeida Campos 150', 'hugo-profile.jpeg');
 
 -- --------------------------------------------------------
 
@@ -125,11 +153,11 @@ INSERT INTO `professores` (`id`, `nome`, `cpf`, `telefone`, `email`, `endereco`,
 
 CREATE TABLE `responsaveis` (
   `id` int(11) NOT NULL,
-  `nome` varchar(100) NOT NULL,
+  `nome` varchar(50) NOT NULL,
   `cpf` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
-  `endereco` varchar(100) NOT NULL
+  `email` varchar(50) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `endereco` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -137,8 +165,9 @@ CREATE TABLE `responsaveis` (
 --
 
 INSERT INTO `responsaveis` (`id`, `nome`, `cpf`, `email`, `telefone`, `endereco`) VALUES
-(1, 'Responsavel 1', '123.981.763-86', 'resp1@email.com', '(12) 31283-1872', 'iuhiuhih'),
-(3, 'Responsavel 1', '333.331.333-33', 'responsavel2@email.com', '(12) 31231-2312', 'Endereço daora');
+(1, 'Katia Silva', '111.111.111-11', 'katia@hotmail.com', '(55) 55555-5555', 'Rua 5'),
+(2, 'Kamilah Souza', '222.222.222-22', 'kamila@hotmail.com', '(22) 22222-2222', 'Rua C'),
+(3, 'Tamara Freitas', '333.333.333-33', 'tamara@hotmail.com', '(33) 33333-3333', 'Rua G');
 
 -- --------------------------------------------------------
 
@@ -148,7 +177,7 @@ INSERT INTO `responsaveis` (`id`, `nome`, `cpf`, `email`, `telefone`, `endereco`
 
 CREATE TABLE `salas` (
   `id` int(11) NOT NULL,
-  `sala` varchar(50) NOT NULL,
+  `sala` varchar(30) NOT NULL,
   `descricao` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -157,9 +186,10 @@ CREATE TABLE `salas` (
 --
 
 INSERT INTO `salas` (`id`, `sala`, `descricao`) VALUES
-(1, 'Sala 2', 'É uma turma bem legal'),
-(2, 'Sala 1', 'Sem descrição'),
-(6, 'Sala 3', 'Nova descricao');
+(1, '101', 'Segunda 09:00'),
+(2, '102', 'Segunda 13:00'),
+(3, '103', 'Segunda 18:00'),
+(5, '104', 'Segunda 22:00');
 
 -- --------------------------------------------------------
 
@@ -170,19 +200,19 @@ INSERT INTO `salas` (`id`, `sala`, `descricao`) VALUES
 CREATE TABLE `secretarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `cpf` varchar(20) NOT NULL,
   `telefone` varchar(20) NOT NULL,
-  `endereco` varchar(100) DEFAULT NULL,
-  `email` varchar(50) NOT NULL
+  `endereco` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `secretarios`
 --
 
-INSERT INTO `secretarios` (`id`, `nome`, `cpf`, `telefone`, `endereco`, `email`) VALUES
-(5, 'Outro', '123.123', '(12) 3123', '123123123', 'email@email.com'),
-(6, 'Secretario 1', '211.111.111-11', '(11) 11111-1111', 'Endereço 1', 'secretario1@email.com');
+INSERT INTO `secretarios` (`id`, `nome`, `email`, `cpf`, `telefone`, `endereco`) VALUES
+(5, 'Marcos Paulo', 'marcos@hotmail.com', '555.555.555-55', '(55) 55555-5555', 'Rua Almeida Campos 145'),
+(6, 'Secretário Teste', 'secretario@hotmail.com', '222.222.222-22', '(22) 22222-2222', 'Rua C');
 
 -- --------------------------------------------------------
 
@@ -195,16 +225,17 @@ CREATE TABLE `tesoureiros` (
   `nome` varchar(50) NOT NULL,
   `cpf` varchar(20) NOT NULL,
   `telefone` varchar(20) NOT NULL,
-  `endereco` varchar(100) DEFAULT NULL,
-  `email` varchar(50) NOT NULL
+  `email` varchar(50) NOT NULL,
+  `endereco` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tesoureiros`
 --
 
-INSERT INTO `tesoureiros` (`id`, `nome`, `cpf`, `telefone`, `endereco`, `email`) VALUES
-(1, 'Tesoureiro 1', '133.333.333-33', '(11) 11111-1111', 'Endereço 1', 'tesoureiro1@email.com');
+INSERT INTO `tesoureiros` (`id`, `nome`, `cpf`, `telefone`, `email`, `endereco`) VALUES
+(1, 'Marcelo Silva', '788.888.888-88', '(88) 88888-8888', 'marcelo@hotmail.com', 'Rua Almeida Campos 150'),
+(2, 'Tesoureiro Teste', '789.555.555-55', '(63) 22222-2222', 'tesoureiro@hotmail.com', 'Rua A');
 
 -- --------------------------------------------------------
 
@@ -221,8 +252,18 @@ CREATE TABLE `turmas` (
   `data_final` date DEFAULT NULL,
   `horario` varchar(30) DEFAULT NULL,
   `dia` varchar(30) DEFAULT NULL,
-  `valor_mensalidade` decimal(7,2) DEFAULT NULL
+  `valor_mensalidade` decimal(7,2) DEFAULT NULL,
+  `ano` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `turmas`
+--
+
+INSERT INTO `turmas` (`id`, `disciplina`, `sala`, `professor`, `data_inicio`, `data_final`, `horario`, `dia`, `valor_mensalidade`, `ano`) VALUES
+(1, 1, 2, 3, '2020-12-01', '2021-12-01', '8:00 as 12:00', 'Sexta-Feira', '80.00', 2020),
+(2, 4, 1, 4, '2020-11-02', '2021-11-02', '13:00 as 17:00', 'Segunda a Sexta', '90.00', 2020),
+(3, 2, 3, 3, '2022-11-26', '2022-11-29', '13:00 as 14:00', 'Segunda-feira', '32.00', 2022);
 
 -- --------------------------------------------------------
 
@@ -244,17 +285,19 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `email`, `senha`, `nivel`) VALUES
-(11, 'Professor 1', '111.111.111-11', 'professor1@email.com', '123', 'professor'),
-(22, 'Professor 2', '222.222.222-22', '2@email.com', '123', 'professor'),
-(23, 'Professor 3', '333.333.333-33', '3@email.com', '123', 'professor'),
-(24, 'Administrador', '000.000.000-00', 'admin', 'admin', 'admin'),
-(25, 'Secretario teste', '211.111.111-12', 'secretario', 'secretario', 'secretaria'),
-(26, 'Tesoureiro 1', '133.333.333-33', 'tesoureiro1@email.com', '123', 'tesouraria'),
-(27, 'Aluno 1', '133.333.333-33', 'aluno1@sistema.com', '123', 'aluno'),
-(28, 'Aluno 1', '133.333.333-33', 'aluno1@sistema.com', '123', 'aluno'),
-(29, 'Aluno 2', '132.222.222-22', 'aluno2@sistema.com', '123', 'aluno'),
-(36, 'Responsavel 1', '333.331.333-33', 'responsavel2@email.com', '123', 'tesouraria'),
-(38, 'ASdAsdasd', '123.123.123-12', 'DSgsfsdf', '123', 'aluno');
+(5, 'Marcos Pedro', '555.555.555-55', 'admin', 'admin', 'Admin'),
+(6, 'Secretário Teste', '222.222.222-22', 'secretario@hotmail.com', '123', 'secretaria'),
+(9, 'Professor Teste', '777.777.777-77', 'professor@hotmail.com', '123', 'professor'),
+(10, 'Felipe Santos', '788.888.888-88', 'felipe@hotmail.com', '123', 'professor'),
+(12, 'Administrador', '000.000.000-00', 'hvfadvocacia@gmail.com', '123', 'Admin'),
+(13, 'Felipe Santos', '788.888.888-88', 'secretaria', 'secretaria', 'secretaria'),
+(14, 'Tesoureiro Teste', '789.555.555-55', 'tesoureiro@hotmail.com', '123', 'secretaria'),
+(15, 'Felipe Santos', '788.888.888-88', 'felipe@hotmail.com', '123', 'aluno'),
+(16, 'Mariano Campos', '789.555.555-55', 'mariano@hotmail.com', '123', 'aluno'),
+(17, 'Marina Silva', '875.555.555-55', 'marina@hotmail.com', '123', 'aluno'),
+(19, 'Rui Costaa', '488.888.888-88', 'rui@hotmail.com', '123', 'aluno'),
+(20, 'Outro aluno', '123.123.131-12', 'giosadig@email.com', '123', 'aluno'),
+(21, 'Teste 123123', '111.111.111-11', 'teste@procemsoeifj.com', '123', 'aluno');
 
 --
 -- Indexes for dumped tables
@@ -276,6 +319,12 @@ ALTER TABLE `disciplinas`
 -- Indexes for table `funcionarios`
 --
 ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `matriculas`
+--
+ALTER TABLE `matriculas`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -328,37 +377,43 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `alunos`
 --
 ALTER TABLE `alunos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `disciplinas`
 --
 ALTER TABLE `disciplinas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `funcionarios`
 --
 ALTER TABLE `funcionarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `matriculas`
+--
+ALTER TABLE `matriculas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `professores`
 --
 ALTER TABLE `professores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `responsaveis`
 --
 ALTER TABLE `responsaveis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `salas`
 --
 ALTER TABLE `salas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `secretarios`
@@ -370,19 +425,19 @@ ALTER TABLE `secretarios`
 -- AUTO_INCREMENT for table `tesoureiros`
 --
 ALTER TABLE `tesoureiros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `turmas`
 --
 ALTER TABLE `turmas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
